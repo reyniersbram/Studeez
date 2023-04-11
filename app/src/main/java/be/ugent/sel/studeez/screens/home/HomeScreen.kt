174c5ad45b2eaecc3d9807e06ed53c7cbbf51f9e
lbarraga <lukas.barragantorres@ugent.be>
2023-04-11 16:20:49 +0200

    #54 added home screen view

--- app/src/main/java/be/ugent/sel/studeez/screens/home/HomeScreen.kt
+++ app/src/main/java/be/ugent/sel/studeez/screens/home/HomeScreen.kt
@@ -1,2 +1,23 @@
 package be.ugent.sel.studeez.screens.home
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import be.ugent.sel.studeez.R
+import be.ugent.sel.studeez.common.composable.BasicButton
+import be.ugent.sel.studeez.common.composable.SecondaryScreenToolbar
+import be.ugent.sel.studeez.common.ext.basicButton
+import be.ugent.sel.studeez.resources
+
+@Composable
+fun HomeScreen(
+    openAndPopUp: (String, String) -> Unit,
+    viewModel: HomeViewModel = hiltViewModel()
+) {
+
+    SecondaryScreenToolbar(title = resources().getString(R.string.home)) {
+        BasicButton(R.string.start_session, Modifier.basicButton()) {
+            viewModel.onStartSessionClick(openAndPopUp)
+        }
+    }
+}
