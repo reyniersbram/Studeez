17ad4c374b9b3b10519ee3a01df0a641e2599401
lbarraga <lukas.barragantorres@ugent.be>
2023-04-11 15:49:00 +0200

    added hilt viewmodel with button onclick

--- app/src/main/java/be/ugent/sel/studeez/screens/home/HomeViewModel.kt
+++ app/src/main/java/be/ugent/sel/studeez/screens/home/HomeViewModel.kt
@@ -1,4 +1,14 @@
 package be.ugent.sel.studeez.screens.home
 
-class HomeViewModel {
+import be.ugent.sel.studeez.domain.LogService
+import be.ugent.sel.studeez.screens.StudeezViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(logService: LogService) : StudeezViewModel(logService) {
+
+    fun onStartSessionClick(openAndPopUp: (String, String) -> Unit) {
+        //openAndPopUp(StudeezDestinations.xxx, StudeezDestinations.HOME_SCREEN)
+    }
 }
