927b99c32ab920c2f6fda5da900c2da95c887a5d
lbarraga <lukas.barragantorres@ugent.be>
2023-04-13 17:20:50 +0200

    #59 UserDAO interface

--- app/src/main/java/be/ugent/sel/studeez/domain/UserDAO.kt
+++ app/src/main/java/be/ugent/sel/studeez/domain/UserDAO.kt
@@ -1,4 +1,14 @@
 package be.ugent.sel.studeez.domain
 
+import kotlinx.coroutines.flow.Flow
+
 interface UserDAO {
+
+    suspend fun getUserName(): String?
+
+    suspend fun save(newUsername: String)
+
+//    suspend fun update(task: Task)
+//    suspend fun delete(taskId: String)
+//    suspend fun deleteAllForUser(userId: String)
 }
