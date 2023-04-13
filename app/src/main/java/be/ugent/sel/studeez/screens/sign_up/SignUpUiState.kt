182b8e22134630a11268480a145a4240d726124c
lbarraga <lukas.barragantorres@ugent.be>
2023-04-13 13:57:17 +0200

    #59 Added username in ui state

--- app/src/main/java/be/ugent/sel/studeez/screens/sign_up/SignUpUiState.kt
+++ app/src/main/java/be/ugent/sel/studeez/screens/sign_up/SignUpUiState.kt
@@ -1,6 +1,7 @@
 package be.ugent.sel.studeez.screens.sign_up
 
 data class SignUpUiState(
+    val username: String = "",
     val email: String = "",
     val password: String = "",
     val repeatPassword: String = ""
