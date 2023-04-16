0af211362d910f82b96e7b56fd4e50b4d987c7f6
tdpeuter <tibo.depeuter@ugent.be>
2023-04-16 09:54:23 +0200

    #14 Username styling

--- app/src/main/java/be/ugent/sel/studeez/common/composable/TextComposable.kt
+++ app/src/main/java/be/ugent/sel/studeez/common/composable/TextComposable.kt
@@ -1,23 +1,26 @@
 package be.ugent.sel.studeez.common.composable
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun Headline(
     text: String
 ) {
     Row (
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
     ) {
         Text(
             text = text,
-            modifier = Modifier.height(45.dp)
+            fontSize = 34.sp
         )
     }
 }
