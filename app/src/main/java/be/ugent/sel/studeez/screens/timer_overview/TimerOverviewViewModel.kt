880236e1ce3981c8d1013eedb231a2b455220e55
lbarraga <lukas.barragantorres@ugent.be>
2023-04-16 23:33:18 +0200

    #40 viewmodel for overview screen

--- app/src/main/java/be/ugent/sel/studeez/screens/timer_overview/TimerOverviewViewModel.kt
+++ app/src/main/java/be/ugent/sel/studeez/screens/timer_overview/TimerOverviewViewModel.kt
@@ -1,4 +1,34 @@
 package be.ugent.sel.studeez.screens.timer_overview
 
-class TimerOverviewViewModel {
+import be.ugent.sel.studeez.data.local.models.timer_info.TimerInfo
+import be.ugent.sel.studeez.domain.ConfigurationService
+import be.ugent.sel.studeez.domain.LogService
+import be.ugent.sel.studeez.domain.TimerDAO
+import be.ugent.sel.studeez.screens.StudeezViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+@HiltViewModel
+class TimerOverviewViewModel @Inject constructor(
+    private val configurationService: ConfigurationService,
+    private val timerDAO: TimerDAO,
+    logService: LogService
+) : StudeezViewModel(logService) {
+
+    fun getUserTimers() : Flow<List<TimerInfo>> {
+        return timerDAO.getUserTimers()
+    }
+
+    fun getDefaultTimers(): List<TimerInfo> {
+        return configurationService.getDefaultTimers()
+    }
+
+    fun update(timerInfo: TimerInfo) = timerDAO.updateTimer(timerInfo)
+
+    fun delete(timerInfo: TimerInfo) =timerDAO.deleteTimer(timerInfo)
+
+    fun save(timerInfo: TimerInfo) = timerDAO.saveTimer(timerInfo)
+
+
 }
