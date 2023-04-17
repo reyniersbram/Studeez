169a7d34692cb2b5229a6da0ab4d243ebc3b1179
Lukas Barragan Torres <lukas.barragantorres@ugent.be>
2023-04-17 10:11:19 +0200

    #65 sessionscreen

--- app/src/main/java/be/ugent/sel/studeez/screens/session/SessionViewModel.kt
+++ app/src/main/java/be/ugent/sel/studeez/screens/session/SessionViewModel.kt
@@ -1,4 +1,20 @@
 package be.ugent.sel.studeez.screens.session
 
-class SessionViewModel {
+import be.ugent.sel.studeez.data.local.models.timer_functional.FunctionalPomodoroTimer
+import be.ugent.sel.studeez.data.local.models.timer_functional.FunctionalTimer
+import be.ugent.sel.studeez.domain.LogService
+import be.ugent.sel.studeez.screens.StudeezViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SessionViewModel @Inject constructor(
+    logService: LogService
+) : StudeezViewModel(logService) {
+
+    private val timer: FunctionalTimer = FunctionalPomodoroTimer(15, 5, 3)
+
+    fun getTimer() : FunctionalTimer {
+        return timer
+    }
 }
