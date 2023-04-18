31e07d2784f78d6fe3005613be5b94ed6ed62d51
Lukas Barragan Torres <lukas.barragantorres@ugent.be>
2023-04-18 15:38:19 +0200

    #76 added sessionReport

--- app/src/main/java/be/ugent/sel/studeez/data/local/models/SessionReport.kt
+++ app/src/main/java/be/ugent/sel/studeez/data/local/models/SessionReport.kt
@@ -1,4 +1,10 @@
 package be.ugent.sel.studeez.data.local.models
 
-class SessionReport {
-}
+import com.google.firebase.Timestamp
+import com.google.firebase.firestore.DocumentId
+
+data class SessionReport(
+    @DocumentId val id: String = "",
+    val studyTime: Int = 0,
+    val endTime: Timestamp = Timestamp(0, 0)
+    )
