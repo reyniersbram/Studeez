8f84c24bad75b8dc6c585923d075f7e5651bc88f
Lukas Barragan Torres <lukas.barragantorres@ugent.be>
2023-04-18 10:41:58 +0200

    singleton repo for selectedTimer

--- app/src/main/java/be/ugent/sel/studeez/data/SelectedTimerRepo.kt
+++ app/src/main/java/be/ugent/sel/studeez/data/SelectedTimerRepo.kt
@@ -1,11 +1,14 @@
-package be.ugent.sel.studeez.screens.timers
+package be.ugent.sel.studeez.data
 
 import be.ugent.sel.studeez.data.local.models.timer_functional.FunctionalTimer
 import javax.inject.Inject
 import javax.inject.Singleton
 
+/**
+ * Used to communicate the SelectedTimer from the selection screen to the session screen.
+ * Because this is a singleton-class the view-models of both screens observe the same data.
+ */
 @Singleton
 class SelectedTimerRepo @Inject constructor(){
     var selectedTimer: FunctionalTimer? = null
-
 }
