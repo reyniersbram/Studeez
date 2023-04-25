96795d0e18e41911956dde97030fedab5f82c473
lbarraga <lukas.barragantorres@ugent.be>
2023-04-25 18:10:55 +0200

    #76 session recap viewmodel en screen

--- app/src/main/java/be/ugent/sel/studeez/screens/session_recap/SessionRecapViewModel.kt
+++ app/src/main/java/be/ugent/sel/studeez/screens/session_recap/SessionRecapViewModel.kt
@@ -1,4 +1,33 @@
 package be.ugent.sel.studeez.screens.session_recap
 
-class SessionRecapViewModel {
+import be.ugent.sel.studeez.data.SessionReportState
+import be.ugent.sel.studeez.data.local.models.SessionReport
+import be.ugent.sel.studeez.domain.LogService
+import be.ugent.sel.studeez.domain.SessionDAO
+import be.ugent.sel.studeez.navigation.StudeezDestinations
+import be.ugent.sel.studeez.screens.StudeezViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SessionRecapViewModel @Inject constructor(
+    private val sessionReportState: SessionReportState,
+    private val sessionDAO: SessionDAO,
+    logService: LogService
+) : StudeezViewModel(logService) {
+
+    private val report: SessionReport = sessionReportState.sessionReport!!
+
+    fun getSessionReport(): SessionReport {
+        return report
+    }
+
+    fun saveSession(open: (String, String) -> Unit) {
+        sessionDAO.saveSession(getSessionReport())
+        open(StudeezDestinations.HOME_SCREEN, StudeezDestinations.SESSION_RECAP)
+    }
+
+    fun discardSession(open: (String, String) -> Unit) {
+        open(StudeezDestinations.HOME_SCREEN, StudeezDestinations.SESSION_RECAP)
+    }
 }
