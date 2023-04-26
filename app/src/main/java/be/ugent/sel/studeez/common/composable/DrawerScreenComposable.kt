ce3261df076a065dad7999a21cb8c6c49020a11c
tdpeuter <tibo.depeuter@ugent.be>
2023-04-26 10:29:14 +0200

    Add DrawerScreen preview

--- app/src/main/java/be/ugent/sel/studeez/common/composable/DrawerScreenComposable.kt
+++ app/src/main/java/be/ugent/sel/studeez/common/composable/DrawerScreenComposable.kt
@@ -7,9 +7,11 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.tooling.preview.Preview
 import be.ugent.sel.studeez.common.composable.drawer.Drawer
 import be.ugent.sel.studeez.common.composable.drawer.DrawerActions
 import be.ugent.sel.studeez.resources
+import be.ugent.sel.studeez.ui.theme.StudeezTheme
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import be.ugent.sel.studeez.R.string as AppText
@@ -48,4 +50,15 @@ fun DrawerScreenTemplate(
     ) {
         content(it)
     }
+}
+
+@Preview
+@Composable
+fun DrawerScreenPreview() {
+    StudeezTheme { DrawerScreenTemplate(
+        title = "Drawer screen preview",
+        drawerActions =DrawerActions({}, {}, {}, {}, {})
+    ) {
+        Text(text = "Preview content")
+    } }
 }
