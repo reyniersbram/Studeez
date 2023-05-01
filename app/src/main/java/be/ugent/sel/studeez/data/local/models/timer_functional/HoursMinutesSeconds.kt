1d6de333e4d9a17fc657b93af7adf22a3aba5a4f
lbarraga <lukas.barragantorres@ugent.be>
2023-05-01 12:53:35 +0200

    HMS now takes ints and has totalseconds and tostring functions

--- app/src/main/java/be/ugent/sel/studeez/data/local/models/timer_functional/HoursMinutesSeconds.kt
+++ app/src/main/java/be/ugent/sel/studeez/data/local/models/timer_functional/HoursMinutesSeconds.kt
@@ -1,4 +1,15 @@
 package be.ugent.sel.studeez.data.local.models.timer_functional
 
-data class HoursMinutesSeconds(val hours: String, val minutes: String, val seconds: String
-)
+data class HoursMinutesSeconds(val hours: Int, val minutes: Int, val seconds: Int) {
+
+    fun getTotalSeconds(): Int {
+        return hours * 60 * 60 + minutes * 60 + seconds
+    }
+
+    override fun toString(): String {
+        val hoursString = hours.toString().padStart(2, '0')
+        val minutesString = minutes.toString().padStart(2, '0')
+        val secondsString = seconds.toString().padStart(2, '0')
+        return "$hoursString : $minutesString : $secondsString"
+    }
+}
