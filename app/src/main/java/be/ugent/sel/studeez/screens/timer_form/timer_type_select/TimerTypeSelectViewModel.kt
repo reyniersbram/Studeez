853a2d8c285ac5e4ed41b24b2c48d87df3329202
lbarraga <lukas.barragantorres@ugent.be>
2023-05-05 00:03:05 +0200

    select timer type string resource and package refactor

--- app/src/main/java/be/ugent/sel/studeez/screens/timer_form/timer_type_select/TimerTypeSelectViewModel.kt
+++ app/src/main/java/be/ugent/sel/studeez/screens/timer_form/timer_type_select/TimerTypeSelectViewModel.kt
@@ -1,13 +1,11 @@
-package be.ugent.sel.studeez.screens.timer_form.timer_add
+package be.ugent.sel.studeez.screens.timer_form.timer_type_select
 
 import be.ugent.sel.studeez.data.EditTimerState
 import be.ugent.sel.studeez.data.local.models.timer_info.TimerInfo
 import be.ugent.sel.studeez.domain.LogService
-import be.ugent.sel.studeez.domain.TimerDAO
 import be.ugent.sel.studeez.navigation.StudeezDestinations
 import be.ugent.sel.studeez.screens.StudeezViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 @HiltViewModel
