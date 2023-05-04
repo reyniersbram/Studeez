51af1f576625818eadf7a6e8776609eb648d6827
lbarraga <lukas.barragantorres@ugent.be>
2023-05-05 00:04:52 +0200

    edit route en add route staan in dezelfde TimerFormRoute File

--- app/src/main/java/be/ugent/sel/studeez/screens/timer_form/TimerFormRoute.kt
+++ app/src/main/java/be/ugent/sel/studeez/screens/timer_form/TimerFormRoute.kt
@@ -1,13 +1,10 @@
-package be.ugent.sel.studeez.screens.timer_form.timer_edit
+package be.ugent.sel.studeez.screens.timer_form
 
 import androidx.annotation.StringRes
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 import be.ugent.sel.studeez.common.composable.SecondaryScreenTemplate
 import be.ugent.sel.studeez.data.local.models.timer_info.TimerInfo
-import be.ugent.sel.studeez.screens.timer_form.GetTimerFormScreen
-import be.ugent.sel.studeez.screens.timer_form.TimerFormViewModel
-import java.util.Timer
 import be.ugent.sel.studeez.R.string as AppText
 
 @Composable
@@ -37,10 +34,10 @@ fun TimerFormScreen(
     @StringRes label: Int,
     onConfirmClick: (TimerInfo) -> Unit
 ) {
-    val timerEditScreen = getTimerInfo().accept(GetTimerFormScreen())
+    val timerFormScreen = getTimerInfo().accept(GetTimerFormScreen())
 
     SecondaryScreenTemplate(title = stringResource(id = label), popUp = popUp) {
-        timerEditScreen(onConfirmClick)
+        timerFormScreen(onConfirmClick)
     }
 }
 
