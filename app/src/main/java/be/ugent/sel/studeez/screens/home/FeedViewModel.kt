190c3467ec78aaf30f535bf4554c2a4f525d8f18
lbarraga <lukas.barragantorres@ugent.be>
2023-05-07 14:03:46 +0200

    #74 viewmodel of a feed

--- app/src/main/java/be/ugent/sel/studeez/screens/home/FeedViewModel.kt
+++ app/src/main/java/be/ugent/sel/studeez/screens/home/FeedViewModel.kt
@@ -1,4 +1,37 @@
 package be.ugent.sel.studeez.screens.home
 
-class FeedViewModel {
+import androidx.lifecycle.viewModelScope
+import be.ugent.sel.studeez.data.SelectedTask
+import be.ugent.sel.studeez.data.local.models.FeedEntry
+import be.ugent.sel.studeez.domain.FeedDAO
+import be.ugent.sel.studeez.domain.LogService
+import be.ugent.sel.studeez.domain.TaskDAO
+import be.ugent.sel.studeez.navigation.StudeezDestinations
+import be.ugent.sel.studeez.screens.StudeezViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class FeedViewModel @Inject constructor(
+    private val feedDAO: FeedDAO,
+    private val taskDAO: TaskDAO,
+    private val selectedTask: SelectedTask,
+    logService: LogService
+) : StudeezViewModel(logService) {
+
+    private val entries: Flow<List<FeedEntry>> = feedDAO.getFeedEntries()
+
+    fun getFeedEntries(): Flow<List<FeedEntry>> {
+        return entries
+    }
+
+    fun continueWithTask(open: (String) -> Unit, subjectId: String, taskId: String) {
+        viewModelScope.launch {
+            val task = taskDAO.getTask(subjectId, taskId)
+            selectedTask.set(task)
+            open(StudeezDestinations.TIMER_SELECTION_SCREEN)
+        }
+    }
 }
