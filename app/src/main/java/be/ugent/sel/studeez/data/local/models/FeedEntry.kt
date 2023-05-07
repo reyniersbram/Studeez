260171a972a1b858e66bd52bcfe9392cdb8c9577
lbarraga <lukas.barragantorres@ugent.be>
2023-05-07 14:25:18 +0200

    #74 feedEntry now has an endTime

--- app/src/main/java/be/ugent/sel/studeez/data/local/models/FeedEntry.kt
+++ app/src/main/java/be/ugent/sel/studeez/data/local/models/FeedEntry.kt
@@ -1,10 +1,13 @@
 package be.ugent.sel.studeez.data.local.models
 
+import com.google.firebase.Timestamp
+
 data class FeedEntry(
     val argb_color: Long = 0,
     val subJectName: String = "",
     val taskName: String = "",
     val taskId: String = "", // Name of task is not unique
     val subjectId: String = "",
-    val totalStudyTime: Int = 0
+    val totalStudyTime: Int = 0,
+    val endTime: Timestamp = Timestamp(0, 0)
 )
