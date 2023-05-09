0fb3125f6a4ce5d33e8c42fbde874b0deb38dc05
Lukas Barragan Torres <lukas.barragantorres@ugent.be>
2023-05-09 16:17:46 +0200

    #74 button to add subject when user has empty feed

--- app/src/main/java/be/ugent/sel/studeez/common/composable/feed/FeedViewModel.kt
+++ app/src/main/java/be/ugent/sel/studeez/common/composable/feed/FeedViewModel.kt
@@ -36,4 +36,8 @@ class FeedViewModel @Inject constructor(
             open(StudeezDestinations.TIMER_SELECTION_SCREEN)
         }
     }
+
+    fun onEmptyFeedHelp(open: (String) -> Unit) {
+        open(StudeezDestinations.ADD_SUBJECT_FORM)
+    }
 }
