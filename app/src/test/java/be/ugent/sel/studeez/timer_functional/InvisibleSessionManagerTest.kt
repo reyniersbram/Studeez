efd92ce832d23d7bd18eb7fcf3119758a9f8a039
brreynie <bram.reyniers@ugent.be>
2023-05-10 09:59:17 +0200

    fix tests unable to compile

--- app/src/test/java/be/ugent/sel/studeez/timer_functional/InvisibleSessionManagerTest.kt
+++ app/src/test/java/be/ugent/sel/studeez/timer_functional/InvisibleSessionManagerTest.kt
@@ -1,9 +1,13 @@
 package be.ugent.sel.studeez.timer_functional
 
 import android.media.MediaPlayer
+import be.ugent.sel.studeez.data.SelectedSessionReport
+import be.ugent.sel.studeez.data.SelectedTimer
 import be.ugent.sel.studeez.data.local.models.timer_functional.FunctionalCustomTimer
 import be.ugent.sel.studeez.data.local.models.timer_functional.FunctionalEndlessTimer
 import be.ugent.sel.studeez.data.local.models.timer_functional.FunctionalPomodoroTimer
+import be.ugent.sel.studeez.domain.LogService
+import be.ugent.sel.studeez.domain.implementation.LogServiceImpl
 import be.ugent.sel.studeez.screens.session.InvisibleSessionManager
 import be.ugent.sel.studeez.screens.session.SessionViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -16,14 +20,14 @@ import org.mockito.kotlin.mock
 
 @ExperimentalCoroutinesApi
 class InvisibleSessionManagerTest {
-    private var timerState: SelectedTimer = SelectedTimer()
+    private var selectedTimer: SelectedTimer = SelectedTimer()
     private lateinit var viewModel: SessionViewModel
     private var mediaPlayer: MediaPlayer = mock()
 
     @Test
     fun InvisibleEndlessTimerTest() = runTest {
-        timerState.selectedTimer = FunctionalEndlessTimer()
-        viewModel = SessionViewModel(timerState, SessionReport(), mock())
+        selectedTimer.set(FunctionalEndlessTimer())
+        viewModel = SessionViewModel(selectedTimer, SelectedSessionReport(), mock(), LogServiceImpl())
         InvisibleSessionManager.setParameters(viewModel, mediaPlayer)
 
         val test = launch {
@@ -44,8 +48,8 @@ class InvisibleSessionManagerTest {
         val studyTime = 10
         val breakTime = 5
         val repeats = 1
-        timerState.selectedTimer = FunctionalPomodoroTimer(studyTime, breakTime, repeats)
-        viewModel = SessionViewModel(timerState, SessionReport(), mock())
+        selectedTimer.set(FunctionalPomodoroTimer(studyTime, breakTime, repeats))
+        viewModel = SessionViewModel(selectedTimer, SelectedSessionReport(), mock(), LogServiceImpl())
         InvisibleSessionManager.setParameters(viewModel, mediaPlayer)
 
         val test = launch {
@@ -77,8 +81,8 @@ class InvisibleSessionManagerTest {
 
     @Test
     fun InvisibleCustomTimerTest() = runTest {
-        timerState.selectedTimer = FunctionalCustomTimer(5)
-        viewModel = SessionViewModel(timerState, SessionReport(), mock())
+        selectedTimer.set(FunctionalCustomTimer(5))
+        viewModel = SessionViewModel(selectedTimer, SelectedSessionReport(), mock(), LogServiceImpl())
         InvisibleSessionManager.setParameters(viewModel, mediaPlayer)
 
         val test = launch {
