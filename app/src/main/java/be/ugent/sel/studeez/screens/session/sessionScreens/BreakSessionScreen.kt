5f0bdc948fc35e9b916f97dc5f3aaf367a7c2a4d
brreynie <bram.reyniers@ugent.be>
2023-05-13 15:57:20 +0200

    all dots green when ended

--- app/src/main/java/be/ugent/sel/studeez/screens/session/sessionScreens/BreakSessionScreen.kt
+++ app/src/main/java/be/ugent/sel/studeez/screens/session/sessionScreens/BreakSessionScreen.kt
@@ -4,18 +4,13 @@ import android.media.MediaPlayer
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import be.ugent.sel.studeez.R
 import be.ugent.sel.studeez.data.local.models.timer_functional.FunctionalPomodoroTimer
 import be.ugent.sel.studeez.resources
 import be.ugent.sel.studeez.R.string as AppText
@@ -23,7 +18,7 @@ import be.ugent.sel.studeez.R.string as AppText
 class BreakSessionScreen(
     private val funPomoDoroTimer: FunctionalPomodoroTimer,
     private var mediaplayer: MediaPlayer?
-): AbstractSessionScreen() {
+) : AbstractSessionScreen() {
 
     @Composable
     override fun MidSection() {
@@ -37,23 +32,31 @@ class BreakSessionScreen(
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Center,
         ) {
-            repeat(funPomoDoroTimer.repeats - funPomoDoroTimer.breaksRemaining - 1) {
-                Dot(color = Color.DarkGray)
-            }
-            if (!funPomoDoroTimer.isInBreak) Dot(Color.Green) else Dot(Color.DarkGray)
-            repeat(funPomoDoroTimer.breaksRemaining) {
-                Dot(color = Color.Gray)
+            if (funPomoDoroTimer.hasEnded()) {
+                repeat(funPomoDoroTimer.repeats) {
+                    Dot(Color.Green)
+                }
+            } else {
+                repeat(funPomoDoroTimer.repeats - funPomoDoroTimer.breaksRemaining - 1) {
+                    Dot(color = Color.DarkGray)
+                }
+                if (!funPomoDoroTimer.isInBreak) Dot(Color.Green) else Dot(Color.DarkGray)
+                repeat(funPomoDoroTimer.breaksRemaining) {
+                    Dot(color = Color.Gray)
+                }
             }
         }
     }
 
     @Composable
     private fun Dot(color: Color) {
-        Box(modifier = Modifier
-            .padding(5.dp)
-            .size(10.dp)
-            .clip(CircleShape)
-            .background(color))
+        Box(
+            modifier = Modifier
+                .padding(5.dp)
+                .size(10.dp)
+                .clip(CircleShape)
+                .background(color)
+        )
     }
 
     @Composable
