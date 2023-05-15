346b24aabe952a0d46f4b5f537901a71771631d6
lbarraga <lukas.barragantorres@ugent.be>
2023-05-15 19:05:43 +0200

    give soundplayer to visitor

--- app/src/main/java/be/ugent/sel/studeez/screens/session/SessionRoute.kt
+++ app/src/main/java/be/ugent/sel/studeez/screens/session/SessionRoute.kt
@@ -5,32 +5,23 @@ import android.media.RingtoneManager
 import android.net.Uri
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
-import be.ugent.sel.studeez.data.local.models.timer_functional.FunctionalPomodoroTimer
 import be.ugent.sel.studeez.data.local.models.timer_functional.FunctionalTimer
-import be.ugent.sel.studeez.screens.session.sessionScreens.AbstractSessionScreen
-import be.ugent.sel.studeez.screens.session.sessionScreens.GetSessionScreen
-import be.ugent.sel.studeez.screens.session.sessionScreens.composables.BreakSessionScreenComposable
 import be.ugent.sel.studeez.screens.session.sessionScreens.composables.GetSessionScreenComposable
 
 data class SessionActions(
     val getTimer: () -> FunctionalTimer,
     val getTask: () -> String,
-    val startMediaPlayer: () -> Unit,
-    val releaseMediaPlayer: () -> Unit,
     val endSession: () -> Unit
 )
 
 private fun getSessionActions(
     viewModel: SessionViewModel,
     openAndPopUp: (String, String) -> Unit,
-    mediaplayer: MediaPlayer,
 ): SessionActions {
     return SessionActions(
         getTimer = viewModel::getTimer,
         getTask = viewModel::getTask,
         endSession = { viewModel.endSession(openAndPopUp) },
-        startMediaPlayer = mediaplayer::start,
-        releaseMediaPlayer = mediaplayer::release,
     )
 }
 
@@ -50,8 +41,9 @@ fun SessionRoute(
         mediaplayer = mediaplayer
     )
 
-    val sessionActions = getSessionActions(viewModel, openAndPopUp, mediaplayer)
-    val sessionScreen = viewModel.getTimer().accept(GetSessionScreenComposable(mediaplayer, open, sessionActions))
+    val soundPlayer = SoundPlayer(LocalContext.current)
+    val sessionActions = getSessionActions(viewModel, openAndPopUp)
+    val sessionScreen = viewModel.getTimer().accept(GetSessionScreenComposable(soundPlayer, open, sessionActions))
 
     sessionScreen()
 }
